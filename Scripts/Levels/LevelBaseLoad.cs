a26a6356ab66fe038e8a02388eda5338e194b0c0
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:56:25 +0300

    Add files via upload

---
+++ Scripts/Levels/LevelBaseLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelBaseLoad : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+            SceneManager.LoadScene("LevelTwo", LoadSceneMode.Single);
+    }
+}
---
+++ Scripts/Levels/LevelTwoLoad.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTwoLoad : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+            SceneManager.LoadScene("LevelTwo", LoadSceneMode.Single);
+    }
+}
