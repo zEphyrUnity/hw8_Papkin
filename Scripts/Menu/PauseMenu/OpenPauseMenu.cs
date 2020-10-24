b8907e392000f65c7be8820daeca45efd1927cc1
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:57:50 +0300

    Add files via upload

---
+++ Scripts/Menu/PauseMenu/OpenPauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class OpenPauseMenu : MonoBehaviour
+{
+    public static string activeScene;
+    public static bool _gameIsPaused = false;
+
+    private void Start()
+    {
+        activeScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel") & !_gameIsPaused)
+        {
+            Debug.Log("Escape");
+            SceneManager.LoadScene("MenuPause", LoadSceneMode.Additive);
+            StartCoroutine(SetMenuPauseActive());
+
+            _gameIsPaused = true;
+            Time.timeScale = 0f;
+        }
+    }
+
+    IEnumerator SetMenuPauseActive()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("SetActive");
+    }
+}
---
+++ Scripts/Menu/PauseMenu/Resume.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Resume : MonoBehaviour
+{
+    public Button yourButton;
+
+    private void Start()
+    {
+        Button resume = yourButton.GetComponent<Button>();
+        resume.onClick.AddListener(Unpause);
+    }
+
+    private void Unpause()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
