5c201a91806b295d6c56bcfbf3e6cdddf6968d4a
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:59:08 +0300

    Add files via upload

---
+++ Scripts/Menu/PauseMenu/OptionsMenu/WeaponVolume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WeaponVolume : MonoBehaviour
+{
+    private Scrollbar _weaponVolume;
+    private GameObject _hero;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _hero = GameObject.FindGameObjectWithTag("Hero");
+        _audioSource = _hero.GetComponent<AudioSource>();
+
+        _weaponVolume = gameObject.GetComponent<Scrollbar>();
+        _weaponVolume.value = 0.2f;
+    }
+
+    private void Update()
+    {
+        _audioSource.volume = _weaponVolume.value;
+    }
+}
