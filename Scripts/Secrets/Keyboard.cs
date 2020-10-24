5c98b2a951cfcf360e80edeab6972c72dd0dc987
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 18:00:38 +0300

    Add files via upload

---
+++ Scripts/Secrets/Keyboard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public class Keyboard : MonoBehaviour
+{
+    private GameObject _barrelBomb;
+    private GameObject _startPosition;
+
+    private void Start()
+    {
+        _barrelBomb = GameObject.FindGameObjectWithTag("Barrel");
+        _startPosition = GameObject.FindGameObjectWithTag("KeyboardSecretStartPosition");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Box"))
+            Invoke("SpawnBombs", 1);
+
+    }
+
+    private void SpawnBombs()
+    {
+        Instantiate(_barrelBomb, _startPosition.transform);
+    }
+}
