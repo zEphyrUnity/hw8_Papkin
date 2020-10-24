7e0b148b0ea1ecd0c9cb1caa91d6c12adb4fbeb1
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 18:01:53 +0300

    Add files via upload

---
+++ Scripts/UI/AmmoText.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+
+public class AmmoText : MonoBehaviour
+{
+    private TextMeshProUGUI _ammoText;
+
+    private void Start()
+    {
+        _ammoText = gameObject.GetComponent<TextMeshProUGUI>();
+        _ammoText.color = Color.yellow;
+    }
+
+    private void Update()
+    {
+        _ammoText.text = $"Ammo {Reinforcement.ammunition}";
+        _ammoText.fontStyle = FontStyles.LowerCase;
+    }
+}
---
+++ Scripts/UI/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private float _lerpSpeed = 0.3f;
+
+    private void Awake()
+    {
+        transform.rotation = Quaternion.LookRotation(Movement.movement);
+        transform.rotation = Quaternion.Euler(20f, 0, 0);
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, GetCurrentPlayerPosition(), _lerpSpeed);
+    }
+
+    private Vector3 GetCurrentPlayerPosition()
+    {
+        return _playerTransform.position + new Vector3(0f, 5f, -10f);
+    }
+}
