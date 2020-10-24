526403fbb97189ddaf6329bd17839643288e5e3a
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:59:35 +0300

    Add files via upload

---
+++ Scripts/Menu/StartMenu/NewGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class NewGame : MonoBehaviour
+{
+    public Button yourButton;
+
+    private void Start()
+    {
+        Button _newGame = yourButton.GetComponent<Button>();
+        _newGame.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("LevelOne", LoadSceneMode.Single);
+    }   
+}
