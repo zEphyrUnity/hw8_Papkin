79486430f378e010556ac8a87e52314f241e3155
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:59:59 +0300

    Add files via upload

---
+++ Scripts/Menu/Exit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    public Button yourButton;
+
+    private void Start()
+    {
+        Button _exitButton = yourButton.GetComponent<Button>();
+        _exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void ExitGame()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+        #else      
+                Application.Quit();
+        #endif
+    }
+}
