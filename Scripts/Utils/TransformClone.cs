abf5ab359d993d58a7f7615c03e30d8c66533cf2
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 18:02:14 +0300

    Add files via upload

---
+++ Scripts/Utils/TransformClone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+namespace Utils
+{
+    public struct TransformData
+    {
+        public Vector3 position;
+        public Vector3 localPosition;
+        public Vector3 localScale;
+        public Quaternion rotation;
+        public Quaternion localRotation;
+        public Transform parent;
+    }
+
+    public static class TransformClone
+    {
+        public static TransformData Clone(this Transform transform)
+        {
+            TransformData td = new TransformData();
+
+            td.position = transform.position;
+            td.localPosition = transform.localPosition;
+            td.localScale = transform.localScale;
+            td.rotation = transform.rotation;
+            td.localRotation = transform.localRotation;
+            td.parent = transform.parent;
+
+            return td;
+        }
+    }
+}
+
