381159d85a1c1e35496ba4acb6c7e58c46f4f824
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 17:55:15 +0300

    Add files via upload

---
+++ Scripts/Gates/GateController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class GateController : MonoBehaviour
+{
+    private Animator _GateAnimator;
+
+    private void Start()
+    {
+        _GateAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+        {
+            _GateAnimator.SetBool("IsGateOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+        {
+            _GateAnimator.SetBool("IsGateOpen", false);
+        }
+    }
+}
---
+++ Scripts/Gates/KalitkaController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class KalitkaController : MonoBehaviour
+{
+    private Animator _kalitkaAnimator;
+
+    private void Start()
+    {
+        _kalitkaAnimator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+            _kalitkaAnimator.SetBool("IsKalitkaOpen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hero"))
+            _kalitkaAnimator.SetBool("IsKalitkaOpen", false);
+    }
+}
