998b5fe556c6814ecd428b2ec9606c5b862081f8
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-10-24 18:01:01 +0300

    Add files via upload

---
+++ Scripts/Shells/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _lifeTime;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        Destroy(gameObject, _lifeTime);
+
+        _rigidbody = GetComponent<Rigidbody>();
+
+        var impulse = transform.up * _rigidbody.mass * _speed;
+
+        _rigidbody.AddForce(impulse, ForceMode.Impulse);
+    }
+}
